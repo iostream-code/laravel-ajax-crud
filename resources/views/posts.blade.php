30cbca4519a7f9203ff51a7553b6bd603e6bc77e
iostream-code <aryakmifjannah2603@gmail.com>
2023-11-26 16:06:41 +0700

    edit title page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel Ajax CRUD - SantriKoding.com</title>
+    <title>Laravel Ajax CRUD</title>
     <style>
         body {
             background-color: lightgray !important;
@@ -14,7 +14,7 @@
     </style>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
